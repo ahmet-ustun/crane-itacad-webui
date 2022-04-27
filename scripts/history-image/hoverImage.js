1d34ac33c0be1a0ae089faa60bb2b284456f016a
ahmet-ustun <ahmetustunt@gmail.com>
2022-04-28 01:42:24 +0300

    small commit

--- scripts/history-image/hoverImage.js
+++ scripts/history-image/hoverImage.js
@@ -3,9 +3,9 @@ $('section.history img').each(function () {
     $(this)
         .on('mouseover', function () {
             $(this).next('figcaption')
-                .animate({ bottom: '0px' }, 300);
+                .animate({ bottom: '0px' }, 200);
         }).on('mouseout', function () {
             $(this).next('figcaption')
-                .animate({ bottom: '-40px' }, 300);
+                .animate({ bottom: '-40px' }, 200);
         });
 });
