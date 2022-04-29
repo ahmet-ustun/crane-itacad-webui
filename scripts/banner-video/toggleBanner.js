92b62b403690008c4d764243677294bacac1aff5
ahmet-ustun <ahmetustunt@gmail.com>
2022-04-29 09:53:43 +0300

    changing the animation to slide

--- scripts/banner-video/toggleBanner.js
+++ scripts/banner-video/toggleBanner.js
@@ -2,10 +2,10 @@ import { toggle, banner } from "./onLoadBanner.js";
 
 toggle.on('change', function() {
     if (toggle.is(':checked')) {
-        banner.show(500);
+        banner.slideDown(500);
         localStorage.setItem('banner-status', 'true');
     } else {
-        banner.hide(500);
+        banner.slideUp(500);
         localStorage.setItem('banner-status', 'false');
     }
 });
